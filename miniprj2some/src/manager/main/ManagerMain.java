7b04caacc134a069ece44c8e91005a5c63cc2cb0
Eunjin <chocoding0@naver.com>
2022-05-23 21:09:05 +0900

    init setting2

---
+++ miniprj2some/src/manager/main/ManagerMain.java
@@ -0,0 +1,10 @@
+package manager.main;
+
+public class ManagerMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("여기는 관리자 부분..!");
+	}
+
+}
---
+++ miniprj2some/src/user/main/UserMain.java
@@ -0,0 +1,9 @@
+package user.main;
+
+public class UserMain {
+
+	public static void main(String[] args) {
+		System.out.println("여기는 사용자 부분..!");
+	}
+
+}
