b660ff1708d78a9281cc5863856d9e44f554a4c7
DESKTOP-AJ76NJ9\pcs1214 <acs>
2022-05-25 11:16:40 +0900

    일단 출력문만 작성했습니다

---
+++ miniprj2some/src/manager/main/ManagerLogin.java
@@ -0,0 +1,23 @@
+package manager.main;
+
+public class ManagerLogin {
+	
+	public void login() {
+		System.out.println("=====로그인=====");
+		System.out.print("아이디 :");
+		System.out.print("패스워드 :");
+	}
+	
+	
+	public void join() {
+		System.out.println("=====회원가입=====");
+		System.out.print("아이디 :");
+		System.out.print("비밀번호 :");
+		System.out.print("이름 :");
+		System.out.println("성별 :");
+		System.out.println("생년월일 :");
+		System.out.println("전화번호 :");
+		System.out.println("이메일 :");
+	}
+
+}
