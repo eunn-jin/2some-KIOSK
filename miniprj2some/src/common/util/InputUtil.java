f6c55f23b81ac51c46537c661030405f3bf90b72
Eunjin <chocoding0@naver.com>
2022-05-25 15:27:44 +0900

    Util클래스 이름변경
    MyUtil에서 InputUtil로 변경했습니다.
    다른 패키지의 util클래스도 삭제했습니다.

--- miniprj2some/src/common/util/InputUtil.java
+++ miniprj2some/src/common/util/InputUtil.java
@@ -1,26 +1,23 @@
-package manager.menu;
+package common.util;
 
 import java.util.Scanner;
 
 public class InputUtil {
+	public static Scanner sc = new Scanner(System.in);
 	
-public static final Scanner sc = new Scanner(System.in);
-	
+	public static int inputInt() {
+		int n = sc.nextInt();
+		sc.nextLine();
+		return n;
+	}
+
 	public static String inputStr() {
 		String str = sc.nextLine();
 		return str;
-	}
-	
-	public static int inputInt() {
-		String str = sc.nextLine();
-		return Integer.parseInt(str);
 	}
 	
 	public static double inputDouble() {
 		String str = sc.nextLine();
 		return Double.parseDouble(str);
 	}
-	
-
-
 }
