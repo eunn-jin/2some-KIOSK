3ada97b848d6d2b49d67c569a71d2eb954139f9d
eunbi <eb4771@naver.com>
2022-05-25 17:15:07 +0900

    0525

--- miniprj2some/src/manager/menu/MenuManager.java
+++ miniprj2some/src/manager/menu/MenuManager.java
@@ -14,6 +14,12 @@ public class MenuManager {
 	public int categoryNum;
 	public int menuNum;	
 	
+	public static void main(String[] args) {
+		
+	
+	}
+	
+	
 	public void showCategory() {
 		System.out.println("=============================");
 		System.out.println("           메뉴 관리 ");
@@ -25,7 +31,7 @@ public class MenuManager {
 		System.out.println();
 		System.out.println("카테고리를 선택하시오");
 		System.out.print("카테고리 번호 : ");
-//		this.categoryNum = InputUtil.sc.nextInt();
+		this.categoryNum = InputUtil.sc.nextInt();
 		System.out.println();
 		System.out.println("----------------------------");
 		System.out.println();
@@ -42,51 +48,51 @@ public class MenuManager {
 		this.categoryNum = categoryNum;
 		
 		
-//		Connection conn = OracleDB.getOracleConnection();
-//		
-//		String sql = "SELECT ROWNUM, MN_NAME "
-//				+ "FROM MENU "
-//				+ "INNER JOIN CATEGORY C ON C.CATEGORY_IDX = MENU.CATEGORY_IDX "
-//				+ "WHERE C.CATEGORY_IDX = ?"; //메뉴에 어떻게 숫자 붙이지? -> 일단 ROWNUM으로 해놨음
-//		PreparedStatement pstmt = null;
-//		ResultSet rs = null;
-//		
-//		try {
-//			pstmt = conn.prepareStatement(sql);
-//			pstmt.setInt(1, categoryNum);
-//			rs = pstmt.executeQuery();
-//			
-//			while(rs.next()) {
-//				   int mIdx = rs.getInt("ROWNUM");
-//				   String mName = rs.getString("MN_NAME");
-//				   System.out.println(mIdx + ". " + mName);
-//		   }
-//			
-//			System.out.println("----------------------------");
-//			System.out.println("[메뉴추가] 00 을 입력하시오");
-//			System.out.println("[메뉴수정] 메뉴 번호를 입력하시오");
-//			System.out.print("입력 : ");
-//			
-//			this.menuNum = InputUtil.sc.nextInt();
-//			System.out.println();
-//			System.out.println();
-//			
-//			if(menuNum == 00) {
-//				plus(); //메뉴추가 페이지로 이동
-//			} else {
-//				update();//해당 메뉴 수정 페이지로 이동
-//			}
-//			
-//			
-//		} catch (SQLException e) {
-//			System.out.println("SQL 예외 발생 - showMenu");
-//			e.printStackTrace();
-//			
-//		} finally {
-//			OracleDB.close(conn);
-//			OracleDB.close(pstmt);
-//			OracleDB.close(rs);
-//		}
+		Connection conn = OracleDB.getOracleConnection();
+		
+		String sql = "SELECT ROWNUM, MN_NAME "
+				+ "FROM MENU "
+				+ "INNER JOIN CATEGORY C ON C.CATEGORY_IDX = MENU.CATEGORY_IDX "
+				+ "WHERE C.CATEGORY_IDX = ?"; //메뉴에 어떻게 숫자 붙이지? -> 일단 ROWNUM으로 해놨음
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, categoryNum);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				   int mIdx = rs.getInt("ROWNUM");
+				   String mName = rs.getString("MN_NAME");
+				   System.out.println(mIdx + ". " + mName);
+		   }
+			
+			System.out.println("----------------------------");
+			System.out.println("[메뉴추가] 00 을 입력하시오");
+			System.out.println("[메뉴수정] 메뉴 번호를 입력하시오");
+			System.out.print("입력 : ");
+			
+			this.menuNum = InputUtil.sc.nextInt();
+			System.out.println();
+			System.out.println();
+			
+			if(menuNum == 00) {
+				plus(); //메뉴추가 페이지로 이동
+			} else {
+				update();//해당 메뉴 수정 페이지로 이동
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println("SQL 예외 발생 - showMenu");
+			e.printStackTrace();
+			
+		} finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+			OracleDB.close(rs);
+		}
 		
 	//db, util 클래스 추가 후 수정
 		
@@ -97,7 +103,10 @@ public class MenuManager {
 		
 		System.out.println("---------- 메뉴 추가 ----------");		
 		System.out.println("( * 표시는 필수 입력입니다 )");		
+		System.out.println();
 		
+		//입력받기
+		System.out.println("");
 		
 		
 		
@@ -131,6 +140,8 @@ public class MenuManager {
 		
 		
 	}
+	
+	
 	
 	
 	
---
+++ miniprj2some/src/manager/menu/RunTest.java
@@ -0,0 +1,15 @@
+package manager.menu;
+
+public class RunTest {
+
+	public static void main(String[] args) {
+		
+		MenuManager mm = new MenuManager();
+		mm.showCategory();
+		
+		
+		
+		
+	}
+
+}
