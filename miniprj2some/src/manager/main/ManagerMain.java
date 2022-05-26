ffa9cbe4e366a8afdc669ec5da4afea669eb6f4b
DESKTOP-AJ76NJ9\pcs1214 <acs>
2022-05-26 21:40:20 +0900

    로그인 수정하였습니다

--- miniprj2some/src/manager/main/ManagerMain.java
+++ miniprj2some/src/manager/main/ManagerMain.java
@@ -1,26 +1,31 @@
 package manager.main;
 
+import java.util.Scanner;
+
 import common.util.InputUtil;
 import manager.main.ManagerLogin;
 
 public class ManagerMain {
 
 	public static void Managermain() {
-
-		System.out.println("=====관리자=====");
 		
-		System.out.println("=================");
-		System.out.println("1. 회원가입");
-		System.out.println("2. 로그인");
 		
-		int n = InputUtil.inputInt();
+		System.out.println("=====관리자 페이지 입니다!=====");
 		
-		switch(n) {
-		case 1 : new ManagerLogin().join(); break;
-		case 2 : new ManagerLogin().login(); break;
-		default : System.out.println("번호를 잘못 입력하셨습니다.");
-		}
-		
+			System.out.println("===========================");
+			System.out.println("1. 회원가입");
+			System.out.println("2. 로그인");
+			System.out.println("3. 로그아웃");
+			
+			int n = InputUtil.inputInt();
+			
+			switch(n) {
+			case 1 : new ManagerLogin().join(); break;
+			case 2 : new ManagerLogin().login(); break;
+			case 3 : new ManagerLogin().logout(); break;
+			default : System.out.println("번호를 잘못 입력하셨습니다.");
+			}
+				
 	}
 
 }
