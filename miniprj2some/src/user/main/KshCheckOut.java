e2dfccea0091cce6d1ca7e600f99724aa56b6c9a
DESKTOP-UB83BMT\Sam <soohwan34@hanmail.net>
2022-05-27 05:07:01 +0900

    주문/계산하기 클래스 거의 완성 주문>추가옵션 미완성

--- miniprj2some/src/user/main/KshCheckOut.java
+++ miniprj2some/src/user/main/KshCheckOut.java
@@ -6,9 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import common.db.OracleDB;
+import common.util.InputUtil;
 
 public class KshCheckOut {
 	
+	KshOrder ko = new KshOrder();
 	
 //	- 메뉴명, 수량, 추가옵션, 가격, 할인가격(쿠폰), 총합 금액 (vat 포함) 보여주기 
 //		1. 디비접근
@@ -25,7 +27,7 @@ public class KshCheckOut {
 		System.out.println("");
 		
 		Connection conn = OracleDB.getOracleConnection();
-		
+		int tPrice = 0;
 		String sql = "SELECT LPAD(ITEM_PRICE,10,' ') AS PRICE FROM ORDER_ITEM";
 		
 		try {
@@ -33,10 +35,10 @@ public class KshCheckOut {
 			ResultSet rs = pstmt.executeQuery();
 			
 			if(rs.next()) {
-				int tPrice = rs.getInt("PRICE");
+				tPrice = rs.getInt("PRICE");
 				
-				System.out.print("                       총 금액 :   ");
-				System.out.println(tPrice + "   원");
+				System.out.print(String.format("%30s", "총 가격") + " : ");
+				System.out.println(String.format("%,7d", tPrice) + " 원");
 				System.out.println(" ");
 			}
 			
@@ -45,18 +47,115 @@ public class KshCheckOut {
 		}
 		showDiscount();
 		
+		System.out.println(" ------------------------------------------- ");
+		System.out.print(String.format("%30s", "최종 결제금액") + " : ");
+		
+		KshOrder ko = new KshOrder();
+		System.out.println(String.format("%,7d", tPrice - ko.getCouval()) + " 원");
+		System.out.println(" ");
+		 
+		proceed();
+		
 	}
 	
-	public void showDiscount() {
+	public int showDiscount() {
 		
 		KshOrder ko = new KshOrder();
 		
-		System.out.print("                        할인금액 :   ");
-		System.out.println(ko.getCouval() + "   원");
-		
-		
+		System.out.print(String.format("%30s", "할인 금액") + " : ");
+		System.out.println(String.format("%,7d", ko.getCouval()) + " 원");
+		return 0;
 		
 	}
 	
+	public int getTotalPrice() {
+		
+		KshOrder ko = new KshOrder();
+		
+		Connection conn = OracleDB.getOracleConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int totalPrice = 0;
+		String sql = "SELECT ITEM_PRICE FROM ORDER_ITEM";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				totalPrice = rs.getInt("ITEM_PRICE");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+			OracleDB.close(rs);
+		}
+		return totalPrice;
+		
+	}
 	
+	public void proceed() {
+		
+		System.out.printf("%30s", "\"Y\" 를 입력하여 계산하기");
+		System.out.printf("\n");
+		
+		String proceed =  InputUtil.inputStr();
+			if(proceed.equalsIgnoreCase("y")) {
+				System.out.println("계산하시려면 결제금액을 입력해주세요.");
+			
+			}else {
+				System.out.println("다시");
+			}
+		
+		int tPrice = InputUtil.inputInt();
+			
+			if(tPrice == (getTotalPrice()-ko.getCouval())) {
+				System.out.println("계산이 완료되었습니다. 안녕히 가세요~");
+			}else {
+				System.out.println("다시 입력해주세요");
+			}
+			
+			inputOrder();
+	}
+	
+	public void inputOrder() {
+	
+		Connection conn = OracleDB.getOracleConnection();
+		
+		//4. '결제하기'로 넘어가면, '주문'테이블에 데이터 추가입력
+		//입력해야 할 데이터 : 주문번호,구입날짜,고객번호,결제금액,할인금액
+		//조회해야 할 테이블 : ORDER_ITEM , CUSTOMER , COUVAR
+		//1)테이블 조인해서 조회
+		//2)조회한 RS 를 다른 곳에 또 담아서 데이터입력(INSERT)를 해야하나??
+		
+		int cpVal = ko.getCouval();
+		PreparedStatement pstmt = null;
+		
+		String sqlSelect = "INSERT INTO ORDER_GROUP"
+				+ "(GROUP_NO, ORDER_DATE, TOTAL_PRICE, DISCOUNT_PRICE)"
+				+ "VALUES"
+				+ "(GROUP_SEQ.NEXTVAL, SYSDATE, ?, ?)";
+		
+		try {
+			pstmt = conn.prepareStatement(sqlSelect);
+			pstmt.setInt(1, getTotalPrice());
+			pstmt.setInt(2, cpVal);
+			int result = pstmt.executeUpdate();
+			
+	
+			if(result == 1) {
+				System.out.println("주문 테이블에 데이터가 정상적으로 입력되었습니다.");
+			}else {
+				System.out.println("데이터 입력 오류ㅠㅠ");
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("jdbc 어딘가 오류,,,");
+		}finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+		}
+	
+	}
 }
