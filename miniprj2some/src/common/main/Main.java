e80cfc0738d6e69601b5102e94016ad407aafa8b
Eunjin <chocoding0@naver.com>
2022-05-27 14:34:47 +0900

    Main class 완성

--- miniprj2some/src/common/main/Main.java
+++ miniprj2some/src/common/main/Main.java
@@ -8,17 +8,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("=================");
+		System.out.println("======== 해당하는 계정을 선택하세요 =========");
 		System.out.println("1. 관리자");
 		System.out.println("2. 유저");
+		System.out.println("0. 종료하기");
 		
-		int n = InputUtil.inputInt();
+		boolean isexit = false;
+		while(!isexit) {
+			System.out.print("선택 : ");
+			int n = InputUtil.inputInt();
 			
-		switch(n) {
-			case 1 : ManagerMain.Managermain(); break;
-			case 2 : UserMain.Usermain(); break;
-			default : System.out.println("번호를 잘못 입력하셨습니다.");
-		}
+			switch(n) {
+				case 1 : ManagerMain.managerMain(); break;
+				case 2 : UserMain.userMain(); break;
+				case 0 : break;
+				default : System.out.println("번호를 잘못 입력하셨습니다."); continue;
+			}
+			isexit = true;
+		}  
 	}
 
 }
